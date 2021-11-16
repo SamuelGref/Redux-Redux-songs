a359eed2c05a9f65494d1edf50faa0d8a797f10a
Samuel Gref <samgref@gmail.com>
2021-11-15 21:14:19 -0500

    Wiring up Provide

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+// Action creater
+
+export const selectSong = (song) => {
+  // return an action
+  return {
+    type: "SONG_SELECTED",
+    payload: song,
+  };
+};
+
---
+++ src/components/SongList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class SongList extends Component {
+  render() {
+    return <div>SongList</div>;
+  }
+}
+
+export default SongList
